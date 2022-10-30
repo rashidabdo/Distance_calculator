4da4f8a44a18c3f16d5ef1921a457272a850ccc0
Rashid abdo <r.abdo@bookerzzz.com>
2022-10-30 15:07:18 +0100

    Add calculator factory to swap between yard and meter unit

---
+++ app/Http/Services/CalculatorFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Services;
+
+class CalculatorFactory
+{
+    protected $model;
+    public function handle($returnUnit)
+    {
+        if (strtolower($returnUnit) == 'meters')
+            return $this->model = new MeterCalculator();
+        else
+            return $this->model = new yardCalculator();
+    }
+}
